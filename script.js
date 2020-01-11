5971faa53745502fb087662afbdec684fdaf8d6a
Lorenzo Marinucci <lorenmarinucci@gmail.com>
2020-01-11 15:23:45 -0300

    Algo de JS y cambios minimos de CSS.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const add = (a, b) => a+b;
+let display="";
+const buttons = document.querySelectorAll("button");
+      displayText = document.querySelector("#displayText");
+      add = (a, b) => a+b;
       subtract = (a, b) => a-b;
       multiply = (a, b) => a*b;
       divide = (a, b) => a/b;
@@ -13,4 +16,17 @@ function operate(a, b, operator) {
     if (!operations[operator]) return alert("Invalid operator");
     return operations[operator](a,b);
 }
-    
+
+function input(e) {
+    if (e.target.classList.contains("number") || e.target.classList.contains("operator"))
+        display+=e.target.id;
+    else if (e.target.classList.contains("equal"))
+            display=evaluate(displayText);
+    console.log(e.target.id);
+    displayText.textContent=display;
+}
+
+buttons.forEach(button => {
+    button.addEventListener('click', input);
+});
+
